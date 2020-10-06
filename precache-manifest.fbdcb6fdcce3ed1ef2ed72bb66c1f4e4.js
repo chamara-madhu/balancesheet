346697910534cda6dec02bdfe8a5df35a7576a0b
Chamara <chamara@spritzer.app>
2020-10-06 20:42:39 +0530

    Updates

---
+++ precache-manifest.fbdcb6fdcce3ed1ef2ed72bb66c1f4e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "733583384673ec2025828948e6725720",
+    "url": "/balancesheet/index.html"
+  },
+  {
+    "revision": "1303e96d463c526bab99",
+    "url": "/balancesheet/static/css/main.3a2f4d68.chunk.css"
+  },
+  {
+    "revision": "cf6fbc8dd0ec1caaf900",
+    "url": "/balancesheet/static/js/2.3b693f6c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/balancesheet/static/js/2.3b693f6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1303e96d463c526bab99",
+    "url": "/balancesheet/static/js/main.d26b32f6.chunk.js"
+  },
+  {
+    "revision": "2d27cc32f01ae4c7a897",
+    "url": "/balancesheet/static/js/runtime-main.4109a00b.js"
+  }
+]);
